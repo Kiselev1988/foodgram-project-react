8ce71bf51178729700f1a1e6f2b97b15b00ebef4
Никита Киселев <chechetkin1988@yandex.ru>
2022-09-22 22:31:03 -0600

    fifth try

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 from rest_framework.validators import UniqueTogetherValidator
 
 from recipes.models import (
