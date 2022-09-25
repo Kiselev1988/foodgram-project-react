fa1d904805e636c17ef163bf420d12883a39a50e
Никита Киселев <chechetkin1988@yandex.ru>
2022-09-25 12:29:07 -0600

    fifth try

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -14,5 +14,5 @@ router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 urlpatterns = [
     path('', include(router_v1.urls)),
     path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.authtoken'))
 ]
